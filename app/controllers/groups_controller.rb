596fef80cffeff0f93804b8e36470654589ad933
captainhz <106868@gmail.com>
2016-12-20 17:30:14 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,52 +1,77 @@
 class GroupsController < ApplicationController
- before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
- before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
-  def index
-    @groups = Group.all
-  end
-  def new
-    @group = Group.new
-  end
-  def show
-    @group = Group.find(params[:id])
-    @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
-  end
-  def edit
+   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destory]
+   before_action :find_group_add_check_permission, only: [:edit, :update, :destroy]
+   def index
+     @groups = Group.all
+    end
+    def new
+      @group = Group.new
+    end
+    def show
+      @group = Group.find(params[:id])
+      @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
+    end
+    def edit
 
     end
-  end
-  def create
-    @group = Group.new(group_params)
-    @group.user = current_user
-    if @group.save
-    redirect_to groups_path
+    def create
+      @group = Group.new(group_params)
+      @group.user = current_user
+      if @group.save
+       current_user.join!(@group)
+       redirect_to groups_path
+     else
+       render :new
+     end
+    end
+    def update
+
+       if @group.update(group_params)
+
+       redirect_to groups_path, notice: "Update Succes!"
+     else
+       render :edit
+     end
+     end
+     def destroy
+
+       @group.destroy
+       flash[:alert] = "Group deleted!"
+       redirect_to groups_path
+     end
+     def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
     else
-     render :new
-   end
-  end
-  def destroy
-
-    @group.destroy
-    flash[:alert] = "Group Delete"
-    redirect_to groups_path
-  end
-  def update
-
-    if @group.update(group_params)
-    redirect_to groups_path, notice: "update Success"
-    else
-      render :edit
+      flash[:warning] = "你已经是本讨论版成员了！"
     end
+
+    redirect_to group_path(@group)
   end
-  private
-  def find_group_and_check_permission
+
+  def quit
     @group = Group.find(params[:id])
 
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
     end
-  end
 
-  def group_params
-    params.require(:group).permit(:title, :description)
+    redirect_to group_path(@group)
   end
+    private
+   def find_group_add_check_permission
+     @group = Group.find(params[:id])
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no Permission"
+     end
+    end
+    def group_params
+      params.require(:group).permit(:title, :description)
+    end
+end
